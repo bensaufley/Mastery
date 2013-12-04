aa16386882ac14bf5cfa78be722ab95f0f944997
Ben Saufley <subscriptions@bensaufley.com>
2013-12-04 00:10:10 -0500

    Adding Devise mailer host to test

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -33,4 +33,7 @@ Mastery::Application.configure do
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
+  
+  # Devise mailer setting
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
